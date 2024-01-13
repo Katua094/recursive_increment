faa791d04c1216a688b9afe7b49e84c5a6d00cfb
David katua <149246422+Katua094@users.noreply.github.com>
2024-01-13 20:58:58 +0300

    Add timer.py to the repository

---
+++ python/timer.py
@@ -0,0 +1,13 @@
+# import time
+
+# for s in range(10,0,-1):
+#     print (s)
+#     time.sleep(1)
+# print("happy new year!!")    
+
+tuple={1,2,3,4,5,6,7,8,8}
+sum=0
+for i in tuple:
+    sum+=i
+
+print(sum)

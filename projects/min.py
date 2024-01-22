14cfc88c0908403e7197b0b29e43f52903c67f66
David katua <149246422+Katua094@users.noreply.github.com>
2024-01-22 21:29:06 +0300

    add min.py to your repository

--- projects/min.py
+++ projects/min.py
@@ -1,7 +1,16 @@
-import calendar
-year=2024
+import time
 
-for month in range(1,13):
-    x=calendar.month(year,month)
-    print(x)
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        seconds -= 1
 
+    print("Timer Complete!")
+
+# Set the desired countdown time in seconds
+countdown_time = 300  # 5 minutes
+
+countdown_timer(countdown_time)

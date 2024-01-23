6c00a6842611167c4ababb526c2174b9b815b8af
David katua <149246422+Katua094@users.noreply.github.com>
2024-01-23 22:21:27 +0300

    add bonga.py to your repository

--- projects/bonga.py
+++ projects/bonga.py
@@ -1,14 +1,38 @@
-import random 
+class Prey:
+    alive = True
 
-random_1=random.choice(1,90)
-while True:
-    force=input("enter to generate a number>>")
-    try:
-        if force:
-            print(random_1)
-        else:
-            force=="quit"
-            break
-    except ValueError:
-        if force=="quit":
-            break
+    def __init__(self, animal_type, species):
+        self.type = animal_type
+        self.species = species
+
+    def killed(self):
+        return f"This {self.type} is getting killed."
+
+
+class Predator:
+    alive = True
+
+    def __init__(self, animal_type, species):
+        self.type = animal_type
+        self.species = species
+
+    def eat(self):
+        return f"This {self.type} is eating."
+
+
+class Rabbit(Prey):
+    def run(self):
+        return f"This {self.type} is running."
+
+
+class Lion(Predator):
+    pass
+
+
+lion_1 = Lion("lion", "panthera leo")
+rabbit = Rabbit("Hare", "mono")
+
+print(lion_1.eat())
+print(rabbit.killed())
+print(lion_1.alive)
+print(rabbit.run())

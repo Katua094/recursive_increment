7688af0a1c5feb07eeebe5c467fb51e171c1f136
David katua <149246422+Katua094@users.noreply.github.com>
2024-01-24 18:46:46 +0300

    add nim.py to your repository

--- projects/nim.py
+++ projects/nim.py
@@ -1,13 +1,12 @@
-print("hello world")
-print("Hello you?\n"+"how is your day going>> ")
+import math 
+print(math.sqrt(81))
+print(round(7/2))
+print(max(39,44,33,456,78,12))
+print(min(39,44,33,456,78,12))
+print(pow(9,3))
+print(math.ceil(9.44433))
+print(math.floor(7.929200202))
+print(abs(39+33))
+print(math.cos(99))
 
-while True:
-    mood = input("yes or No: ")
-    if mood.lower() == "yes":
-        print("good, no need to be sad")
-        break
-    elif mood.lower() == "no":
-        print("nobody cares, work harder!!")
-        break
-    else:
-        print("Please enter 'yes' or 'no'")
+

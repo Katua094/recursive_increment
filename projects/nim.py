31f923ff2c22bea5ce76874e876bcd6706ce78c0
David katua <149246422+Katua094@users.noreply.github.com>
2024-01-26 20:55:54 +0300

    add nim.py to your repository

--- projects/nim.py
+++ projects/nim.py
@@ -1,12 +1,13 @@
-import math 
-print(math.sqrt(81))
-print(round(7/2))
-print(max(39,44,33,456,78,12))
-print(min(39,44,33,456,78,12))
-print(pow(9,3))
-print(math.ceil(9.44433))
-print(math.floor(7.929200202))
-print(abs(39+33))
-print(math.cos(99))
+import os
 
+path="C:\\Users\\Admin\\OneDrive\\Desktop\\bola.txt"
 
+if os.path.isdir(path):
+    print("There is a directory")
+else:
+    print("Nop")
+
+if os.path.isfile(path):
+    print("It is a file !!")
+if os.path.exists(path):
+    print("it exists")

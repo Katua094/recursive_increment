e187ee3e1e38c0fddbe91de1de12d6fafa6ee50c
David katua <149246422+Katua094@users.noreply.github.com>
2024-01-26 21:13:20 +0300

    add min.py to your repository

--- projects/min.py
+++ projects/min.py
@@ -1,16 +1,6 @@
-import time
+#Reading a file with python 
 
-def countdown_timer(seconds):
-    while seconds:
-        mins, secs = divmod(seconds, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
-        time.sleep(1)
-        seconds -= 1
-
-    print("Timer Complete!")
-
-# Set the desired countdown time in seconds
-countdown_time = 300  # 5 minutes
-
-countdown_timer(countdown_time)
+path="C:\\Users\\Admin\\OneDrive\\Desktop\\bola.txt"
+with open(path,"r")as file:
+  for line in file:
+    print(line.strip())

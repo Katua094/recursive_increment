8ef44465a8a697de0ddf7296bc98481d0be53354
David katua <149246422+Katua094@users.noreply.github.com>
2024-02-06 10:16:32 +0300

    add nim.py to your repository

--- projects/nim.py
+++ projects/nim.py
@@ -1,14 +1,27 @@
-# Car game, my guy
-print("Hello! Drive, stop, or quit\n")
+import threading    
+import time
 
-while True:
-    game = input("Your game, bro: ").lower()
 
-    if game == "drive":
-        print("The car is driving!!\n")
-    elif game == "stop":
-        print("The car has stopped\n")
-    elif game == "quit":
-        break
-    else:
-        print("Invalid input. Please enter 'drive', 'stop', or 'quit'.\n")
+def numbers():
+    
+    for i in range(5):
+        time.sleep(1)
+        print(f"thread1:{i}")
+    
+def letters():
+    
+    for i in "ABCDE":
+        time.sleep(1)
+        print(f"thread2:{i}")
+
+thread1=threading.Thread(target=numbers)
+thread2=threading.Thread(target=letters)
+
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+
+
+print("The program has finished running")
+

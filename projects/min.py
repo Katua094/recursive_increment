7a72a85b1a9de879c92563be46039449b932eb0b
David katua <149246422+Katua094@users.noreply.github.com>
2024-02-12 21:51:58 +0300

    add min.py to your repository

--- projects/min.py
+++ projects/min.py
@@ -1,12 +1,35 @@
-#sort()
-#sorted_function
+from tkinter import *
 
-students=(
-    ("markson",19,"nigga"),
-    ("amiyo",18,"uloop")
-    
-)
-age=lambda ages:ages[1]
-sorted_students=sorted(students,key=age,reverse=True)
-for i in sorted_students:
-    print(i)
+food=["pizza","hamburger","chapati"]
+def order():
+    if (x.get()==0):
+        print("you choosed pizza")
+    elif (x.get()==1):
+        print("you ordered a hamburger")
+    elif (x.get()==2):
+        print("you ordered chapati fam")
+
+window=Tk()
+x=IntVar()
+for index in range(len(food)):
+    radio_button=Radiobutton(window,
+                             text=food[index],
+                             variable=x,
+                             value=index,
+                             padx=10,
+                             pady=10,
+                             font=('Arial',50),
+                             indicatoron=0,
+                             width=20,
+                             command=order,
+                             bg="black",
+                             fg="#00ff00",
+                             activebackground="black",
+                             activeforeground="#00ff00"
+                             
+
+                             )
+    radio_button.pack(anchor=W)
+
+
+window.mainloop()

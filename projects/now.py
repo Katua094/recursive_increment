2ebe8eb3f55f088b7d5b951fa78a11788c32b6bb
David katua <149246422+Katua094@users.noreply.github.com>
2024-02-14 16:26:19 +0300

    add now.py to your repository

--- projects/now.py
+++ projects/now.py
@@ -1,26 +1,27 @@
-import threading
-import time
+from tkinter import *
+def submit():
+    print(f"the tempreture is {scale.get()} degres celcius.")
+window=Tk()
+# fire=PhotoImage(file="img.png")
+# label=Label(window,image=fire)
+# label.pack()
+scale=Scale(window,
+            from_=100,
+            to_=0,
+            font=('consolas',15),
+            length=300,
+            tickinterval=10,
+            resolution=5,
+            troughcolor="#69EAFF",
+            fg="red",
+            bg="#111111")
+button=Button(window,text="submit",command=submit)
 
-def print_numbers():
-    for i in range(5):
-        time.sleep(1)  # Simulate some work
-        print(f"Thread 1: {i}")
+scale.set(((scale['from']-scale['to'])/2)+scale['to'])
+scale.pack()
+button.pack()
+# ice=PhotoImage(file='img.png')
+# label_2=Label(window,image=ice)
+# label_2.pack()
 
-def print_letters():
-    for letter in 'ABCDE':
-        time.sleep(1)  # Simulate some work
-        print(f"Thread 2: {letter}")
-
-# Create two threads
-thread1 = threading.Thread(target=print_numbers)
-thread2 = threading.Thread(target=print_letters)
-
-# Start the threads
-thread1.start()
-thread2.start()
-
-# Wait for both threads to finish
-thread1.join()
-thread2.join()
-
-print("Both threads have finished.")
+window.mainloop()
